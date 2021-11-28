30d71c5b057dfc8e75883d2d71cb065fca908870
John Peterson <peterson.glock17@gmail.com>
2021-11-28 09:20:08 -0700

    Removed seeding from startup.
    There are issues with the seeders running multiple times

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ npm install &&
 echo "composer install" &&
 composer install &&
 
-echo "php artisan migrate --seed" &&
-php artisan migrate --seed &&
+echo "php artisan migrate" &&
+php artisan migrate &&
 
 echo "php-fpm" &&
 php-fpm
