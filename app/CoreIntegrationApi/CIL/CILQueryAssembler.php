494bf27705e9f7b23b893bde58bf17b6d2576ecc
Russell Moore <russell.moore@fox-pest.com>
2022-01-06 17:31:53 -0700

    saved some more changes

--- app/CoreIntegrationApi/CIL/CILQueryAssembler.php
+++ app/CoreIntegrationApi/CIL/CILQueryAssembler.php
@@ -20,13 +20,19 @@ class CILQueryAssembler implements QueryAssembler
         // start laravel builder
         // $this->queryBuilder
 
-        // add includes
-
-        // loop over column arguments
-            // $clauseBuilder = $this->clauseBuilderFactory->getClauseBuilder($type);
-            // $this->queryBuilder = $clauseBuilder->build($this->queryBuilder, $column, $value)
+        // loop over types arguments
+            // ? https://laravel.com/docs/8.x/queries#where-clauses
+            // select - all in one - $data = ['name', 'email']
+            // column - Individually - $data = [$column, $value]
+            // includes - all in one - $data = ['tags', 'categories', 'invoices'] - $books = Book::with(['author', 'publisher'])->get(); // ? https://laravel.com/docs/8.x/eloquent-relationships
+            // order by - Individually - $data = name
+            // Don't know exactly how to do method calls yet
+            // id - all in one - $data = 1 or 1,2,3,4,5
+            // perPageParameter - set Local variable, continue, go to next item in the loop, Set perPageParameter Later in paginate($perPageParameter), default 30
+                // $clauseBuilder = $this->clauseBuilderFactory->getClauseBuilder($type);
+                // $this->queryBuilder = $clauseBuilder->build($this->queryBuilder, $data)
         
         // return query;
-        // return $this->queryBuilder->get();
+        // return $this->queryBuilder->paginate($perPageParameter)
     }
 }
--- app/CoreIntegrationApi/CIL/ClauseBuilderFactory.php
+++ app/CoreIntegrationApi/CIL/ClauseBuilderFactory.php
@@ -2,10 +2,78 @@
 
 namespace App\CoreIntegrationApi\CIL;
 
+use App\CoreIntegrationApi\CIL\ClauseBuilder\DateWhereClauseBuilder;
+use App\CoreIntegrationApi\CIL\ClauseBuilder\OrderByClauseBuilder;
+use App\CoreIntegrationApi\CIL\ClauseBuilder\SelectClauseBuilder;
+use App\CoreIntegrationApi\CIL\ClauseBuilder\StringWhereClauseBuilder;
+
 class ClauseBuilderFactory
 {
-    public function getClauseBuilder($type)
+    private $clauseBuilderClass;
+    private $parameterType;
+
+    public function getClauseBuilder($parameterType)
     {
+        $this->parameterType = $parameterType;
+
         // find and return type
+        $this->checkForSelect(); // *** use this method. This will help break up the big if statement.
+        // $this->checkForOrderBy();
+        // $this->checkForDate();
+        // etc.
+
+        return $this->clauseBuilderClass;
+
+
+        // some code to work with
+        if ($parameterType == 'select') {
+            return new SelectClauseBuilder();
+        } elseif ($parameterType == 'orderBy') {
+            return new OrderByClauseBuilder();
+        } elseif (
+            $parameterType == 'date' || 
+            $parameterType == 'timestamp' || 
+            $parameterType == 'datetime' || 
+            str_contains($parameterType, 'date')
+        ) {
+            return new DateWhereClauseBuilder();
+        } elseif (
+            str_contains($parameterType, 'varchar') || 
+            str_contains($parameterType, 'char') || 
+            $parameterType == 'blob' || 
+            $parameterType == 'text'
+        ) {
+            return new StringWhereClauseBuilder();
+        } elseif (
+            $parameterType == 'integer' ||
+            $parameterType == 'int' ||
+            $parameterType == 'smallint' ||
+            $parameterType == 'tinyint' ||
+            $parameterType == 'mediumint' ||
+            $parameterType == 'bigint'
+        ) {
+            return 'int';
+        } elseif (
+            $parameterType == 'decimal' ||
+            $parameterType == 'numeric' ||
+            $parameterType == 'float' ||
+            $parameterType == 'double'
+        ) {
+            return 'float';
+        } else { // Maybe throw an exception here ???
+            foreach ($this->currentParameter as $parameter => $value) {
+                unset($this->paramsAccepted[$parameter]);
+                $this->paramsRejected[$parameter] = "Column type for \"{$parameter}\" is not supported in the query processor, contact the API administer for help!";
+            }
+            return false;
+        } 
+        
     }  
+
+    private function checkForSelect()
+    {
+        if (!$this->clauseBuilderClass && $this->parameterType == 'select') {
+            $this->clauseBuilderClass = new SelectClauseBuilder();
+        }
+    }
 }
