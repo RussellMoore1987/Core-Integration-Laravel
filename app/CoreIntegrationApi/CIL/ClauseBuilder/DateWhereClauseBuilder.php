fb04a62afaea49111139ec0631c6ce09a09d8081
Russell Moore <russell.moore@fox-pest.com>
2022-01-19 15:35:39 -0700

    made some changes

--- app/CoreIntegrationApi/CIL/ClauseBuilder/DateWhereClauseBuilder.php
+++ app/CoreIntegrationApi/CIL/ClauseBuilder/DateWhereClauseBuilder.php
@@ -11,11 +11,13 @@ class DateWhereClauseBuilder implements ClauseBuilder
     private $columnName;
     private $date;
     private $dateAction;
+    private $comparisonOperator;
 
     public function build(Builder $queryBuilder, $data) : Builder
     {
         $this->setMainVariables($queryBuilder, $data);
-        $this->processDate();
+        $this->processDateData();
+        $this->buildDateClause();
 
         return $this->queryBuilder;
     } 
@@ -29,51 +31,55 @@ class DateWhereClauseBuilder implements ClauseBuilder
         $this->date = $date;
     }
 
-    private function processDate()
+    private function processDateData()
+    {
+        $this->processDateString();
+        $this->setComparisonOperator();
+    }
+
+    private function processDateString()
     {
         if (str_contains($this->date, '::')) {
-            $this->parseDateStringWithOperator();
-            $this->buildDateClause();
+            $date_array = explode('::', $this->date);
+    
+            $this->dateAction = strtolower($date_array[1]);
+    
+            if (str_contains($date_array[0], ',')) {
+                $between_dates = explode(',', $date_array[0]);
+                $this->date[] = date('Y-m-d', strtotime($between_dates[0])); // Beginning of day - default 1970-01-01
+                $this->date[] = date('Y-m-d H:i:s', strtotime("tomorrow", strtotime($between_dates[1])) - 1); // End of day - default 1970-01-01 23:59:59
+            } else {
+                $this->date = date('Y-m-d', strtotime($date_array[0]));
+            }
+
         } else {
             $this->date = date('Y-m-d', strtotime($this->date));
-            $this->queryBuilder->whereDate($this->columnName, $this->date);
         }
     }
 
-    private function parseDateStringWithOperator()
+    private function setComparisonOperator()
     {
-        $date_array = explode('::', $this->date);
-
-        $this->dateAction = strtolower($date_array[1]);
-
-        if (str_contains($date_array[0], ',')) {
-            $between_dates = explode(',', $date_array[0]);
-            $this->date[] = date('Y-m-d', strtotime($between_dates[0])); // Beginning of day - default 1970-01-01
-            $this->date[] = date('Y-m-d H:i:s', strtotime("tomorrow", strtotime($between_dates[1])) - 1); // End of day - default 1970-01-01 23:59:59
+        if (in_array($this->dateAction, ['greaterthan', 'gt'])) {
+            $this->comparisonOperator = '>';
+        } else if (in_array($this->dateAction, ['greaterthanorequal', 'gte'])) {
+            $this->comparisonOperator = '>=';
+        } else if (in_array($this->dateAction, ['lessthan', 'lt'])) {
+            $this->comparisonOperator = '<';
+        } else if (in_array($this->dateAction, ['lessthanorequal', 'lte'])) {
+            $this->comparisonOperator = '<=';
+        } else if (in_array($this->dateAction, ['between', 'bt'])) {
+            $this->comparisonOperator = 'bt';
         } else {
-            $this->date = date('Y-m-d', strtotime($date_array[0]));
+            $this->comparisonOperator = '=';
         }
     }
 
     private function buildDateClause()
     {
-        if (in_array($this->dateAction, ['greaterthan', 'gt'])) {
-            $comparisonOperator = '>';
-        } else if (in_array($this->dateAction, ['greaterthanorequal', 'gte'])) {
-            $comparisonOperator = '>=';
-        } else if (in_array($this->dateAction, ['lessthan', 'lt'])) {
-            $comparisonOperator = '<';
-        } else if (in_array($this->dateAction, ['lessthanorequal', 'lte'])) {
-            $comparisonOperator = '<=';
-        } else if (in_array($this->dateAction, ['between', 'bt'])) {
+        if ($this->comparisonOperator == 'bt') {
             $this->queryBuilder->whereBetween($this->columnName, $this->date);
-            return true;
         } else {
-            $comparisonOperator = '=';
+            $this->queryBuilder->whereDate($this->columnName, $this->comparisonOperator, $this->date);
         }
-
-        // split into two functions
-        
-        $this->queryBuilder->whereDate($this->columnName, $comparisonOperator, $this->date);
     }
 }
