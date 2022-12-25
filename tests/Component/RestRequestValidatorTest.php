5af019950c705a455d43d72cbca8c3d180a5dce2
RussellMoore <russell.moore@fox-pest.com>
2022-12-24 21:40:41 -0700

    added refactors

--- tests/Component/RestRequestValidatorTest.php
+++ tests/Component/RestRequestValidatorTest.php
@@ -3,34 +3,34 @@
 namespace Tests\Component;
 
 use App\CoreIntegrationApi\EndpointValidator;
-use Tests\TestCase;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 use App\CoreIntegrationApi\ValidatorDataCollector;
 use App\CoreIntegrationApi\RequestMethodTypeValidatorFactory\RequestMethodTypeValidatorFactory;
 use App\CoreIntegrationApi\RestApi\RestRequestValidator;
 use App\CoreIntegrationApi\RestApi\RestRequestDataPrepper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Tests\TestCase;
 
 class RestRequestValidatorTest extends TestCase
 {
     // TODO: refactor this ???
     /**
-     * @dataProvider returnsExpectedResultProvider
+     * @dataProvider requestMethodProvider
      * @group get
      * @group post
      * @group put
      * @group patch
      * @group delete
      */
-    public function test_RestRequestValidator_returns_expected_result($requestMethod)
+    public function test_RestRequestValidator_returns_expected_result_array_with_correct_keys($requestMethod)
     {
-        $validatedMetaData = $this->validateRequest([
+        $validatedMetaData = $this->getRestRequestValidatorResponse([
             'resource' => 'projects',
             'id' => 33,
             'title' => 'Test Project',
         ], 'api/v1/projects', $requestMethod);
-
-        // just asserting structure, details tested in ResourceModelInfoProvider and ResourceParameterInfoProviders
+        
+        // * asserting structure, details tested in other classes like ResourceModelInfoProvider, ResourceParameterInfoProviders, etc.
         $this->assertArrayHasKey('endpointData', $validatedMetaData);
         $this->assertArrayHasKey('resourceInfo', $validatedMetaData);
         $this->assertArrayHasKey('acceptableParameters', $validatedMetaData['resourceInfo']);
@@ -41,7 +41,7 @@ class RestRequestValidatorTest extends TestCase
         $this->assertArrayHasKey('queryArguments', $validatedMetaData);
     }
 
-    public function returnsExpectedResultProvider()
+    public function requestMethodProvider()
     {
         return [
             'GET' => ['GET'],
@@ -52,7 +52,7 @@ class RestRequestValidatorTest extends TestCase
         ];
     }
 
-    protected function validateRequest(array $parameters = [], $url = 'api/v1/projects', $method = 'GET')
+    protected function getRestRequestValidatorResponse(array $parameters = [], $url = 'api/v1/projects', $method = 'GET')
     {
         $request = Request::create($url, $method, $parameters);
         $restRequestDataPrepper = new RestRequestDataPrepper($request);
