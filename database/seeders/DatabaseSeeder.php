547515697dccf499bc785e58ee128a0669a16967
RussellMoore <russell.moore@fox-pest.com>
2023-01-06 16:28:42 -0700

    made some more security changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,8 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run() : void
     {
         \App\Models\User::factory(3)->create();
         \App\Models\Content::factory(50)->create();
@@ -43,16 +41,16 @@ class DatabaseSeeder extends Seeder
         // Connection tables / pivot tables
         $caseStudies->each(function ($caseStudy) use ($images, $tags, $categories) {
             $caseStudy->images()->attach(
-                $images->random(rand(2, 5))->pluck('id')->toArray(),
-                ['sort_order' => rand(1, 100)]
+                $images->random(random_int(2, 5))->pluck('id')->toArray(),
+                ['sort_order' => random_int(1, 100)]
             );
 
             $caseStudy->tags()->attach(
-                $tags->random(rand(2, 6))->pluck('id')->toArray()
+                $tags->random(random_int(2, 6))->pluck('id')->toArray()
             );
 
             $caseStudy->categories()->attach(
-                $categories->random(rand(2, 6))->pluck('id')->toArray()
+                $categories->random(random_int(2, 6))->pluck('id')->toArray()
             );
 
             $caseStudy->setFeaturedImage($caseStudy->images[0]);
@@ -60,16 +58,16 @@ class DatabaseSeeder extends Seeder
 
         $projects->each(function ($project) use ($images, $tags, $categories) {
             $project->images()->attach(
-                $images->random(rand(2, 5))->pluck('id')->toArray(),
-                ['sort_order' => rand(1, 100)]
+                $images->random(random_int(2, 5))->pluck('id')->toArray(),
+                ['sort_order' => random_int(1, 100)]
             );
 
             $project->tags()->attach(
-                $tags->random(rand(2, 6))->pluck('id')->toArray()
+                $tags->random(random_int(2, 6))->pluck('id')->toArray()
             );
 
             $project->categories()->attach(
-                $categories->random(rand(2, 6))->pluck('id')->toArray()
+                $categories->random(random_int(2, 6))->pluck('id')->toArray()
             );
 
             $project->setFeaturedImage($project->images[0]);
@@ -77,16 +75,16 @@ class DatabaseSeeder extends Seeder
 
         $posts->each(function ($post) use ($images, $tags, $categories) {
             $post->images()->attach(
-                $images->random(rand(2, 5))->pluck('id')->toArray(),
-                ['sort_order' => rand(1, 100)]
+                $images->random(random_int(2, 5))->pluck('id')->toArray(),
+                ['sort_order' => random_int(1, 100)]
             );
 
             $post->tags()->attach(
-                $tags->random(rand(2, 6))->pluck('id')->toArray()
+                $tags->random(random_int(2, 6))->pluck('id')->toArray()
             );
 
             $post->categories()->attach(
-                $categories->random(rand(2, 6))->pluck('id')->toArray()
+                $categories->random(random_int(2, 6))->pluck('id')->toArray()
             );
 
             $post->setFeaturedImage($post->images[0]);
@@ -94,16 +92,16 @@ class DatabaseSeeder extends Seeder
 
         $resources->each(function ($resource) use ($images, $tags, $categories) {
             $resource->images()->attach(
-                $images->random(rand(2, 5))->pluck('id')->toArray(),
-                ['sort_order' => rand(1, 100)]
+                $images->random(random_int(2, 5))->pluck('id')->toArray(),
+                ['sort_order' => random_int(1, 100)]
             );
 
             $resource->tags()->attach(
-                $tags->random(rand(2, 6))->pluck('id')->toArray()
+                $tags->random(random_int(2, 6))->pluck('id')->toArray()
             );
 
             $resource->categories()->attach(
-                $categories->random(rand(2, 6))->pluck('id')->toArray()
+                $categories->random(random_int(2, 6))->pluck('id')->toArray()
             );
 
             $resource->setFeaturedImage($resource->images[0]);
