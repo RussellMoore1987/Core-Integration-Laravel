2611c325539e471b13b08a2fcf9ecc7446ec3c45
RussellMoore <russell.moore@fox-pest.com>
2023-05-19 16:10:21 -0600

    refactored ActionFinder

--- app/CoreIntegrationApi/ParameterValidatorFactory/ParameterValidators/ActionFinder.php
+++ app/CoreIntegrationApi/ParameterValidatorFactory/ParameterValidators/ActionFinder.php
@@ -4,32 +4,30 @@ namespace App\CoreIntegrationApi\ParameterValidatorFactory\ParameterValidators;
 
 use App\CoreIntegrationApi\ParameterValidatorFactory\ParameterValidators\ErrorCollector;
 
-// TODO: relook at this class
-
 class ActionFinder
 {
-    public function parseValue(string $requestValue, ErrorCollector &$errorCollector)
+    public function parseValue(string $parameterValue, ErrorCollector &$errorCollector): array
     {
-        if (str_contains($requestValue, '::')) {
-            $requestValueArray = explode('::', $requestValue);
+        if (str_contains($parameterValue, '::')) {
+            $parameterValueArray = explode('::', $parameterValue);
     
-            $originalComparisonOperator = trim($requestValueArray[1]);
-            $action = strtolower(trim($requestValueArray[1]));
-            $value = trim($requestValueArray[0]);
+            $originalComparisonOperator = trim($parameterValueArray[1]);
+            $action = strtolower($originalComparisonOperator);
+            $value = trim($parameterValueArray[0]);
     
-            if (count($requestValueArray) > 2) {
+            if (count($parameterValueArray) > 2) {
                 $errorCollector->add([
-                    'value' => $requestValue,
+                    'value' => $parameterValue,
                     'valueError' => "Only one comparison operator is permitted per parameter, ex: 123::lt.",
                 ]);
-                unset($requestValueArray[0]);
+                unset($parameterValueArray[0]);
                 $action = 'inconclusive';
-                $originalComparisonOperator = $requestValueArray;
+                $originalComparisonOperator = $parameterValueArray;
             }
         }
 
         return [
-            $value ?? $requestValue,
+            $value ?? $parameterValue,
             $action ?? null,
             $originalComparisonOperator ?? null
         ];
