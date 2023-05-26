c82f247ce3b3961458ef2e8dac39f60c36973363
RussellMoore <russell.moore@fox-pest.com>
2023-05-26 17:27:14 -0600

    Got CREATE DATABASE IF NOT EXISTS to work on build

---
+++ docker/mysql/provision_databases.sql
@@ -0,0 +1,6 @@
+# create databases
+CREATE DATABASE IF NOT EXISTS `coreintegrationdb`;
+CREATE DATABASE IF NOT EXISTS `testing`;
+
+# create root user and grant rights
+GRANT ALL ON *.* TO 'root'@'%';
