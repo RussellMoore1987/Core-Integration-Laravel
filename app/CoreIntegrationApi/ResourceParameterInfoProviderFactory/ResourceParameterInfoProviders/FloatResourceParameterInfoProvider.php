dc64ff1f51d183853b861c70244785208773476b
RussellMoore <russell.moore@fox-pest.com>
2023-06-27 17:15:44 -0600

    added FloatResourceParameterInfoProvider, working on FloatResourceParameterInfoProvider

--- app/CoreIntegrationApi/ResourceParameterInfoProviderFactory/ResourceParameterInfoProviders/FloatResourceParameterInfoProvider.php
+++ app/CoreIntegrationApi/ResourceParameterInfoProviderFactory/ResourceParameterInfoProviders/FloatResourceParameterInfoProvider.php
@@ -1,5 +1,9 @@
 <?php
 
+// TODO: Need to look over chat GPT created
+// TODO: Also look at FloatResourceParameterInfoProviderTest.php
+// ! Start here ******************************************************************
+
 namespace App\CoreIntegrationApi\ResourceParameterInfoProviderFactory\ResourceParameterInfoProviders;
 
 use App\CoreIntegrationApi\ResourceParameterInfoProviderFactory\ResourceParameterInfoProviders\ResourceParameterInfoProvider;
@@ -7,10 +11,138 @@ use App\CoreIntegrationApi\ResourceParameterInfoProviderFactory\ResourceParamete
 class FloatResourceParameterInfoProvider extends ResourceParameterInfoProvider
 {
     protected $apiDataType = 'float';
+    protected $floatType;
+    protected $floatParameterDataType;
+    protected $precision;
+    protected $scale;
 
     protected function setParameterData(): void
     {
-        $this->formData = ['min' => -128];
-        $this->defaultValidationRules = ['min:-128'];
+        $this->prepareFloatParameterForProcessing();
+        $this->isFloatThenSetParameterInfo();
+        $this->isDoubleThenSetParameterInfo();
+        $this->isDecimalThenSetParameterInfo();
+
+        // dd(
+        //     $this->floatParameterDataType,
+        //     $this->parameterDataType,
+        //     $this->precision,
+        //     $this->scale,
+        //     $this->defaultValidationRules,
+        //     $this->formData
+        // );
+    }
+
+    protected function prepareFloatParameterForProcessing(): void
+    {
+        // does it have a precision and scale?
+        if (str_contains($this->parameterDataType, '(')) {
+            $firstParen = strpos($this->parameterDataType, '(');
+            $lastParen = strrpos($this->parameterDataType, ')');
+            $precisionAndScale = substr($this->parameterDataType, $firstParen + 1, $lastParen - $firstParen - 1); // decimal(8,2) = 8,2
+            $precisionAndScale = explode(',', $precisionAndScale);
+            $this->precision = $precisionAndScale[0];
+            $this->scale = $precisionAndScale[1];
+
+            $this->floatParameterDataType = substr($this->parameterDataType, 0, $firstParen); // decimal(8,2) = decimal
+        }
+
+        $this->floatParameterDataType = $this->floatParameterDataType ?? $this->parameterDataType;
+    }
+
+    protected function isFloatThenSetParameterInfo(): void
+    {
+        if ($this->floatTypeIsNotSet() && $this->isFloatType('float')) {
+            $this->setFloatTypeAsTrue();
+            $this->setPrecisionAndScaleIfNotSet(24, 0);
+
+            // TODO: make sure Laravel validation rules are correct
+            $this->defaultValidationRules = [
+                'numeric',
+                'min:' . (-pow(10, $this->precision - $this->scale - 1)),
+                'max:' . (pow(10, $this->precision - $this->scale) - pow(10, -$this->scale)),
+            ];
+
+            $this->formData = [
+                'min' => (-pow(10, $this->precision - $this->scale - 1)),
+                'max' => (pow(10, $this->precision - $this->scale) - pow(10, -$this->scale)),
+                'type' => 'number',
+            ];
+        }
+    }
+
+    protected function isDoubleThenSetParameterInfo(): void
+    {
+        if ($this->floatTypeIsNotSet() && $this->isFloatType('double')) {
+            $this->setFloatTypeAsTrue();
+            $this->setPrecisionAndScaleIfNotSet(53, 0);
+
+            $this->defaultValidationRules = [
+                'numeric',
+                'min:' . (-pow(10, $this->precision - $this->scale - 1)),
+                'max:' . (pow(10, $this->precision - $this->scale) - pow(10, -$this->scale)),
+            ];
+
+            $this->formData = [
+                'min' => (-pow(10, $this->precision - $this->scale - 1)),
+                'max' => (pow(10, $this->precision - $this->scale) - pow(10, -$this->scale)),
+                'type' => 'number',
+            ];
+        }
+    }
+
+    // https://dev.mysql.com/doc/refman/8.0/en/fixed-point-types.html
+    protected function isDecimalThenSetParameterInfo(): void
+    {
+        if ($this->floatTypeIsNotSet() && $this->isFloatTypeDecimalOrNumeric()) {
+            $this->setFloatTypeAsTrue();
+            $this->setPrecisionAndScaleIfNotSet(10, 0);
+
+            $precision = str_repeat('9', $this->precision - $this->scale);
+            $scale = str_repeat('9', $this->scale);
+            $decimal = $precision;
+            if ($this->scale > 0) {
+                $decimal .= '.' . $scale;
+            }
+
+            $this->defaultValidationRules = [
+                'numeric',
+
+                'min: -' . $decimal,
+                'max: ' . $decimal,
+            ];
+
+            $this->formData = [
+                'min' => (-$decimal),
+                'max' => $decimal,
+                'type' => 'number',
+            ];
+        }
+    }
+
+    protected function isFloatTypeDecimalOrNumeric(): bool
+    {
+        return $this->isFloatType('decimal') || $this->isFloatType('numeric');
+    }
+
+    protected function floatTypeIsNotSet(): bool
+    {
+        return !$this->floatType;
+    }
+
+    protected function isFloatType($floatString): bool
+    {
+        return str_contains($this->parameterDataType, $floatString) ? true : false;
+    }
+
+    protected function setFloatTypeAsTrue(): void
+    {
+        $this->floatType = true;
+    }
+
+    protected function setPrecisionAndScaleIfNotSet($precision, $scale): void
+    {
+        $this->precision = $this->precision ?? $precision;
+        $this->scale = $this->scale ?? $scale;
     }
 }
---
+++ tests/Unit/ResourceParameterInfoProvider/FloatResourceParameterInfoProviderTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Tests\Unit\ResourceParameterInfoProvider;
+
+use App\CoreIntegrationApi\ResourceParameterInfoProviderFactory\ResourceParameterInfoProviders\FloatResourceParameterInfoProvider;
+
+use Tests\TestCase;
+
+class FloatResourceParameterInfoProviderTest extends TestCase
+{
+    protected $floatResourceParameterInfoProvider;
+    protected $parameterAttributeArray;
+    protected $minValidationForUnsigned = 'min:0';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->parameterAttributeArray = [
+            'field' => 'fakeParameterName',
+            'type' => 'decimal(10,2)',
+            'null' => 'no',
+            'key' => '',
+            'default' => '0',
+            'extra' => '',
+        ];
+
+        $this->floatResourceParameterInfoProvider = new FloatResourceParameterInfoProvider();
+    }
+
+    /**
+     * @dataProvider decimalResourceParameterInfoProvider
+     * @group rest
+     * @group context
+     * @group allRequestMethods
+     */
+    public function test_floatResourceParameterInfoProvider_returns_correct_values(string $parameterAttributeType, array $expectedResultPieces): void
+    {
+        $this->parameterAttributeArray['type'] = $parameterAttributeType;
+        $result = $this->floatResourceParameterInfoProvider->getData($this->parameterAttributeArray, []);
+
+        $expectedResultPieces['defaultValidationRules'] = [
+            'numeric',
+            'min: ' . $expectedResultPieces['min'],
+            'max: ' . $expectedResultPieces['max'],
+        ];
+        $this->assertEquals($this->getExpectedResult($expectedResultPieces), $result);
+    }
+
+    // TODO make sure tests fail
+    public function decimalResourceParameterInfoProvider(): array
+    {
+        return [
+            'defaultDecimal' => [
+                'decimal',
+                [
+                    'min' => -9999999999,
+                    'max' => 9999999999,
+                ],
+            ],
+            // 'defaultDecimalUnsigned' => [
+            //     'decimal unsigned', // what does this look like in the database?
+            //     [
+            //         'min' => 0,
+            //         'max' => 9999999999,
+            //     ],
+            // ],
+            'decimalWithPrecision_10,2' => [
+                'decimal(10,2)',
+                [
+                    'min' => -99999999.99,
+                    'max' => 99999999.99,
+                ],
+            ],
+            // 'decimalWithPrecision_10,2Unsigned' => [
+            //     'decimal(10,2) unsigned',
+            //     [
+            //         'min' => 0,
+            //         'max' => 9999999999.99,
+            //     ],
+            // ],
+            'decimalWithPrecision_10,0' => [
+                'decimal(10,0)',
+                [
+                    'min' => -9999999999,
+                    'max' => 9999999999,
+                ],
+            ],
+            'decimalWithPrecision_5,3' => [
+                'decimal(5,3)',
+                [
+                    'min' => -99.999,
+                    'max' => 99.999,
+                ],
+            ],
+        ];
+    }
+
+    protected function getExpectedResult(array $expectedResultPieces): array
+    {
+        return [
+            'apiDataType' => 'float',
+            'formData' => [
+                'min' => $expectedResultPieces['min'],
+                'max' => $expectedResultPieces['max'],
+                'type' => 'number',
+            ],
+            'defaultValidationRules' => $expectedResultPieces['defaultValidationRules'],
+        ];
+    }
+}
