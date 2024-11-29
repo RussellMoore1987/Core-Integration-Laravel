1e3e99e41241b6430c3611666e7d545267d26d3e
Russell Moore <truthandgoodness87@gmail.com>
2024-11-29 10:50:00 -0700

    made some changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ composer install &&
 echo "php artisan migrate" &&
 php artisan migrate &&
 
-echo "php artisan migrate testing db" &&
+echo "php artisan migrate --database=testing" &&
 php artisan migrate --database=testing &&
 
 echo "php-fpm" &&
