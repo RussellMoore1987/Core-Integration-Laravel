ebae243217d8ce5dce764e9e49e6ce9765570d5d
Russell Moore <truthandgoodness87@gmail.com>
2024-12-11 14:43:17 -0700

    did some clean up

--- tests/Integration/GetDefaultParamsTest.php
+++ tests/Integration/GetDefaultParamsTest.php
@@ -145,10 +145,6 @@ class GetDefaultParamsTest extends TestCase
 
     }
 
-    // ! start here ***************************************************************************** code review commits from 1e3e99e41241b6430c3611666e7d545267d26d3e to 490532c357a45c3e1b949dda888d8dd6b4959cef
-    // page validation
-    // per_page validation
-
     private function createProjects(): void
     {
         $this->project1 = Project::factory()->create([
