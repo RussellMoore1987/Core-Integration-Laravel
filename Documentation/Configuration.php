2a6f16a0bd4467910ee3c932dd1b15a11569bcf9
Russell Moore <truthandgoodness87@gmail.com>
2025-01-24 17:26:32 -0700

    update configuration and documentation

--- Documentation/Configuration.php
+++ Documentation/Configuration.php
@@ -2,3 +2,12 @@
 
 // @DefaultReturnRequestStructure
 // set in config/coreintegration.php, look at EndpointValidator.php > setMainPortionOfEndpointData
+// dev note: this effects lots of tests
+// default can only be one of the following:
+    // fullInfo
+    // dataOnly
+// parameter to effect request
+    // dataOnly => return only the data requested
+    // fullInfo => return all information for the resource
+    // formData => return only the form data for the resource
+    // columnData => return only the column data for the resource
--- config/coreintegration.php
+++ config/coreintegration.php
@@ -42,15 +42,7 @@ return [
     |
     */
 
-    'defaultReturnRequestStructure' => 'dataOnly', // dev note: this effects lots of tests
-    // default can only be one of the following:
-        // fullInfo
-        // dataOnly
-    // parameter to effect request
-        // dataOnly => return only the data requested
-        // fullInfo => return all information for the resource
-        // formData => return only the form data for the resource
-        // columnData => return only the column data for the resource
+    'defaultReturnRequestStructure' => 'dataOnly', // @DefaultReturnRequestStructure
 
     // TODO:
     // Available methods (ex: GET, POST) per endpoint
