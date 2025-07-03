1236a854aec1bb9b276329f25d6e708993ab4d6b
Russell Moore <russell.moore@fox-pest.com>
2025-07-03 14:55:21 -0600

    made some changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,10 @@ echo "composer install" &&
 composer install &&
 
 echo "php artisan migrate" &&
-php artisan migrate &&
+php artisan migrate --force &&
 
 echo "php artisan migrate --database=testing" &&
-php artisan migrate --database=testing &&
+php artisan migrate --database=testing --force &&
 
 echo "php-fpm" &&
 php-fpm
